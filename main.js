a99fb5f43ad39bf607ee52aa09337e42c7005772
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2021-09-18 00:38:03 +0300

    update

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ let formSearch = document.querySelector(".form-search");
 let formSearchInput = document.querySelector(".form-search__input-text");
 
 const todaysDate = new Date();
-
+const fromDate = new Date(new Date().getTime() - (7 * 24 * 60 * 60 * 1000));
 
 formSearch.addEventListener('submit', retrieve);
 
@@ -56,23 +56,31 @@ function retrieve(e) {
 
    e.preventDefault()
 
-   let apiKey = "1bb2c66fe49f4cc8aae2c07724edd0bd";
+   let apiKey = "2213f1e36fb8400bb4eef4e632efffe3";
 
    let topic = formSearchInput.value;
 
-   let url = `https://nomoreparties.co/news/v2/everything?q=${topic}&from=${todaysDate}&sortBy=publishedAt&apiKey=${apiKey}`;
+   let url = `https://nomoreparties.co/news/v2/everything?q=${topic}&from=${fromDate}&to=${todaysDate}&sortBy=publishedAt&pageSize=100&apiKey=${apiKey}`;
 
    fetch(url).then((res) => {
       return res.json()
    }).then((data) => {
 
+      localStorage.setItem('url', JSON.stringify(data));
+
       let search = document.querySelector(".search");
+
+      if (topic.length === 0) {
+         alert("«Нужно ввести ключевое слово»");
+         return
+      }
 
       if (search.classList.contains("search")) {
          search.classList.add("search__active")
       } else {
          console.log("error")
       }
+
 
       let news = data.articles;
 
@@ -91,63 +99,123 @@ function retrieve(e) {
       })
 
       // adding a photo News API
+      function getUrlImg() {
 
-      let newsImg = document.querySelectorAll('.search__img');
-      for (let i = 0; i < newsImg.length; i++) {
-         newsImg[i].setAttribute("src", imgNews[i])
+         let newsImg = document.querySelectorAll('.search__img');
+         for (let i = 0; i < newsImg.length; i++) {
+            newsImg[i].setAttribute("src", imgNews[i])
+         }
+
       }
+      getUrlImg()
+
 
       // adding a url News API
+      function getUrl() {
 
-      let searchItem = document.querySelectorAll(".search__item");
-      for (let i = 0; i < searchItem.length; i++) {
-         searchItem[i].setAttribute("href", urlNews[i])
-         searchItem[i].setAttribute("target", "_blank")
+         let searchItem = document.querySelectorAll(".search__item");
+         for (let i = 0; i < searchItem.length; i++) {
+            searchItem[i].setAttribute("href", urlNews[i])
+            searchItem[i].setAttribute("target", "_blank")
+         }
+
       }
+
+      getUrl()
+
 
       // adding a title News API
 
-      let searchTitle = document.querySelectorAll('.search__heading');
-      for (let i = 0; i < searchTitle.length; i++) {
-         searchTitle[i].textContent = titleNews[i];
+      function getTitle() {
+
+         let searchTitle = document.querySelectorAll('.search__heading');
+         for (let i = 0; i < searchTitle.length; i++) {
+            searchTitle[i].textContent = titleNews[i];
+         }
+
       }
+
+      getTitle() 
 
       // adding a description News API
 
-      let searchDiscription = document.querySelectorAll('.search__discription');
+      function getDescription() {
+
+         let searchDiscription = document.querySelectorAll('.search__discription');
       for (let i = 0; i < searchDiscription.length; i++) {
          searchDiscription[i].textContent = descriptionNews[i];
+         }
 
       }
-
+      getDescription()
+      
       // adding a source name News API
+      function getSourse() {
 
-      let searchSource = document.querySelectorAll('.search__source-of-text');
-      for (let i = 0; i < searchSource.length; i++) {
-         searchSource[i].textContent = sourseNameNews[i];
+         let searchSource = document.querySelectorAll('.search__source-of-text');
+         for (let i = 0; i < searchSource.length; i++) {
+            searchSource[i].textContent = sourseNameNews[i];
+          }
 
       }
+      getSourse()
+     
 
-
+      // show more News
 
       let btnMoreNews = document.querySelector(".search__button");
-      btnMoreNews.addEventListener("click", function moreNews() {
-   
-      let img = document.createElement("img");
-      img.className = "search__img"
-      document.body.appendChild(img)
-      console.log(img)
+      btnMoreNews.addEventListener("click", moreNews)
 
-      })
 
-      
+      function moreNews(e) {
+
+         e.preventDefault()
+
+         let searchItems = document.querySelector(".search-items");
+         for (let i = 0; i <= 2; i++) {
+
+            let moreNewsItem = document.createElement("a");
+            moreNewsItem.className = "search__item";
+            searchItems.append(moreNewsItem)
+         }
+
+
+         let searchItem = document.querySelectorAll(".search__item");
+         for (let i = 0; i < searchItem.length; i++) {
+
+            // searchItem[i].setAttribute("href", urlNews[i])
+
+            // searchItem[i].setAttribute("target", "_blank")
+
+            let img = document.createElement('img')
+
+            img.className = "search__img"
+
+            if (searchItem[i].firstChild === null) {
+
+               searchItem[i].prepend(img)
+
+            }
+
+         }
+
+         getUrlImg()
+         getUrl()
+
+
+
+
+
+      }
+
+
+
 
 
 
 
    })
 
-   
+
 
 }
-
