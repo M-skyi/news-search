c382eb619c1e97b73693c4a4996a1437ec84f45c
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2021-10-17 16:01:56 +0300

    update

--- analitice.js
+++ analitice.js
@@ -9,22 +9,24 @@ topicValue.textContent = `Вы спросили: «${topicValueLocal}»`;
 
 let weekCounterNews = localStorage.getItem('url');
 weekCounterNews = JSON.parse(weekCounterNews);
+console.log(weekCounterNews)
 
 let totalResults = weekCounterNews.totalResults;
+console.log(totalResults)
 let totalResultsString = document.querySelector(".counter-week__count");
 let stringConter = totalResults.toString().slice(0, 2) + " " + totalResults.toString().slice(2, 5);
 totalResultsString.textContent = stringConter
 
 // number of title per week
+
 let titleNews = weekCounterNews.articles;
 titleNews.forEach(el => {
-    console.log(el.title)
+
+
 });
 
 
 
-
-console.log(titleNews)
 
 
 
